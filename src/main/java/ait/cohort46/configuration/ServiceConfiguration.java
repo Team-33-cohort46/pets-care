d2895931b3652a3908407bf52418a894f22c7dc3
EKil <loreleyko@gmail.com>
2024-12-02 16:52:35 +0100

    Added environment variables

--- src/main/java/ait/cohort46/configuration/ServiceConfiguration.java
+++ src/main/java/ait/cohort46/configuration/ServiceConfiguration.java
@@ -34,7 +34,10 @@ public class ServiceConfiguration {
         return S3Client.builder()
                 .region(Region.US_EAST_1) // Укажите регион вашего бакета
                 .credentialsProvider(StaticCredentialsProvider.create(
-                        AwsBasicCredentials.create("AKIAVA5YK6N6SAA6EWPI", "vhDFvrtHqPWEiIntw7OuPEdAWq83aseOLphZJCJT") // Ваши ключи
+                        AwsBasicCredentials.create(
+                                System.getenv("AWS_ACCESS_KEY"),
+                                System.getenv("AWS_SECRET_KEY")
+                        ) // Ваши ключи
                 ))
                 .build();
     }

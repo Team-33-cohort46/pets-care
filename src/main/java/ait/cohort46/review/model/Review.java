092e740ff8c59ad9256d7efffbe032f2fe3985cc
Team-33-cohort46 <daniil.turovskiy24@gmail.com>
2024-12-10 22:40:41 +0100

    Update Review.java

--- src/main/java/ait/cohort46/review/model/Review.java
+++ src/main/java/ait/cohort46/review/model/Review.java
@@ -1,9 +1,6 @@
 package ait.cohort46.review.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
